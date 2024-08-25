343c1a6ea7f5320f70a791930e0346902c1ee398
Loonmos <demi.marsman@gmail.com>
2024-08-25 18:53:47 +0200

    tried fixing the drip, ending scene, tut scene, level 3 changes, yada yada

--- Echolocation/Assets/Scripts/DripSpawner.cs
+++ Echolocation/Assets/Scripts/DripSpawner.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class DripSpawner : MonoBehaviour
 {
-    private float Timer;
+    private float dripTimer = 0.1f;
     public int timeToDrip;
-    public GameObject drip; 
+    private bool timeForNewDrip = true;
+    public GameObject drip;
 
+    public Animator anim;
+    private float animTimer;
+    public float animTime;
 
     void Update()
     {
-        Timer += Time.deltaTime;
-        if(Timer >= timeToDrip)
+        if (dripTimer == 0)
         {
-            Instantiate(drip, transform);
-            Timer = 0;
+            animTimer = 0;
+            anim.SetBool("Forming", true);
+
+            animTimer += Time.deltaTime;
+
+            if (animTimer >= animTime)
+            {
+                anim.SetBool("Forming", false);
+                Instantiate(drip, transform.position, Quaternion.identity);
+                timeForNewDrip = true;
+            }
+        }
+
+        if (timeForNewDrip == true)
+        {
+            dripTimer += Time.deltaTime;
+
+            if (dripTimer >= timeToDrip + Random.Range(0,5))
+            {
+                dripTimer = 0;
+                timeForNewDrip = false;
+            }
         }
     }
 }
