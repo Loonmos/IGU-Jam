44ce29b8d44d1b66c0f3cfe83731cf9a2885acb5
Loonmos <demi.marsman@gmail.com>
2024-08-25 22:35:45 +0200

    fixed the drip

--- Echolocation/Assets/Scripts/DripSpawner.cs
+++ Echolocation/Assets/Scripts/DripSpawner.cs
@@ -4,40 +4,58 @@ using UnityEngine;
 
 public class DripSpawner : MonoBehaviour
 {
-    private float dripTimer = 0.1f;
+    private float dripTimer = 0;
     public int timeToDrip;
     private bool timeForNewDrip = true;
     public GameObject drip;
 
     public Animator anim;
-    private float animTimer;
-    public float animTime;
+    private float animTimer = 0;
+    public float timeForAnim;
 
     void Update()
     {
-        if (dripTimer == 0)
-        {
-            animTimer = 0;
-            anim.SetBool("Forming", true);
+        //if (dripTimer == 0)
+        //{
+        //    animTimer = 0;
+        //    anim.SetBool("Forming", true);
 
-            animTimer += Time.deltaTime;
+        //    animTimer += Time.deltaTime;
 
-            if (animTimer >= animTime)
-            {
-                anim.SetBool("Forming", false);
-                Instantiate(drip, transform.position, Quaternion.identity);
-                timeForNewDrip = true;
-            }
-        }
+        //    if (animTimer >= animTime)
+        //    {
+        //        anim.SetBool("Forming", false);
+        //        Instantiate(drip, transform.position, Quaternion.identity);
+        //        timeForNewDrip = true;
+        //    }
+        //}
 
         if (timeForNewDrip == true)
         {
             dripTimer += Time.deltaTime;
 
-            if (dripTimer >= timeToDrip + Random.Range(0,5))
+            if (dripTimer >= timeToDrip + Random.Range(0, 5))
             {
                 dripTimer = 0;
                 timeForNewDrip = false;
+            }
+
+
+        }
+
+        if (timeForNewDrip == false)
+        {
+            anim.SetBool("Forming", true);
+
+            animTimer += Time.deltaTime;
+            Debug.Log(animTimer);
+
+            if (animTimer >= timeForAnim)
+            {
+                animTimer = 0;
+                anim.SetBool("Forming", false);
+                Instantiate(drip, transform.position, Quaternion.identity);
+                timeForNewDrip = true;
             }
         }
     }
